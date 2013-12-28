d341a93a0abac593e8da27abfee8dc14dabf522b
Geoff Hensley <geoffhensley@gmail.com>
2013-12-28 09:32:13 -0600

    Event.js - reorganized structure

--- httpdocs/themes/third_party/acs_bridge/event.js
+++ httpdocs/themes/third_party/acs_bridge/event.js
@@ -3,66 +3,74 @@
 	templates['event'] = template(function (Handlebars,depth0,helpers,partials,data) {
 		this.compilerInfo = [4,'>= 1.0.0'];
 		helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+		
+		// Set initial functions
 		var buffer = "", stack1, stack2, stack3, options, functionType="function", escapeExpression=this.escapeExpression, self=this, helperMissing=helpers.helperMissing;
 		
+		// Details
 		function program1(depth0,data) {
-  
-  
-			return "\n				<h2>Details</h2>\n				";
+			// If extensive information exists
+			return "<h2>Details</h2>";
 		}
 
+		// Image
 		function program3(depth0,data) {
-  
 			var buffer = "", stack1;
-			buffer += "\n				<img src=\"";
-			if (stack1 = helpers.Image) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-			else { stack1 = depth0.Image; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
-			buffer += escapeExpression(stack1)
-			+ "\" alt=\"";
-			if (stack1 = helpers.EventName) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-			else { stack1 = depth0.EventName; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
-			buffer += escapeExpression(stack1)
-			+ "\" align=\"left\">\n				";
+			buffer += "<img src='";
+				// set the source
+				if (stack1 = helpers.Image) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+				else { stack1 = depth0.Image; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+				buffer += escapeExpression(stack1)
+				// set alt tag
+		+		"' alt='";
+					if (stack1 = helpers.EventName) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+					else { stack1 = depth0.EventName; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+				buffer += escapeExpression(stack1)
+				// Finish out the img tag with an align left
+		+ 		"' align='left'>";
 			return buffer;
 		}
 
+		// Contact Info
 		function program5(depth0,data) {
-  
 			var buffer = "", stack1;
-			buffer += "<br>	\n				<h2>Contact Information</h2>\n				";
+			// set header
+			buffer += "<br><h2>Contact Information</h2>";
 			stack1 = helpers.each.call(depth0, depth0.Sections, {hash:{},inverse:self.noop,fn:self.program(6, program6, data),data:data});
 			if(stack1 || stack1 === 0) { buffer += stack1; }
-			buffer += "\n				<br>\n				";
+			buffer += "<br>";
 			return buffer;
 		}
+		
+		// In Depth Contact Info
 		function program6(depth0,data) {
-  
 			var buffer = "", stack1;
-			buffer += "\n				<strong>Email:</strong> ";
-			if (stack1 = helpers.Contactemail) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-			else { stack1 = depth0.Contactemail; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+			buffer += "<strong>Email:</strong> ";
+				if (stack1 = helpers.Contactemail) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+				else { stack1 = depth0.Contactemail; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
 			buffer += escapeExpression(stack1)
-			+ "<br />\n				<strong>Phone:</strong> ";
-			if (stack1 = helpers.ContactPhone) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-			else { stack1 = depth0.ContactPhone; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+			+	"<br><strong>Phone:</strong> ";
+				if (stack1 = helpers.ContactPhone) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+				else { stack1 = depth0.ContactPhone; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
 			buffer += escapeExpression(stack1)
-			+ "<br />\n				";
+			+	"<br>";
 			return buffer;
 		}
 
+		// Set Registration
 		function program8(depth0,data) {
-  
 			var buffer = "", stack1;
-			buffer += "\n				<h2>Registration Periods</h2>\n				";
+			buffer += "<h2>Registration Periods</h2>";
 			stack1 = helpers.each.call(depth0, depth0.RegistrationPeriods, {hash:{},inverse:self.noop,fn:self.program(9, program9, data),data:data});
 			if(stack1 || stack1 === 0) { buffer += stack1; }
-			buffer += "\n						\n				<a href=\"https://secure.accessacs.com/access/login_guest.aspx?sn=106649\"><button class=\"pull-right\">Register Now</button></a>\n				";
+			buffer += "<a href='https://secure.accessacs.com/access/login_guest.aspx?sn=106649'><button class='pull-right'>Register Now</button></a>";
 			return buffer;
 		}
+		
+		// Registration 
 		function program9(depth0,data) {
-  
 			var buffer = "", stack1, options;
-			buffer += "\n				";
+			buffer += " ";
 			if (stack1 = helpers.Name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
 			else { stack1 = depth0.Name; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
 			buffer += escapeExpression(stack1)
@@ -76,7 +84,7 @@
 			+ " to ";
 			options = {hash:{},data:data};
 			buffer += escapeExpression(((stack1 = helpers.dateFormat || depth0.dateFormat),stack1 ? stack1.call(depth0, depth0.EndTime, options) : helperMissing.call(depth0, "dateFormat", depth0.EndTime, options)))
-			+ "\n				";
+			+ " ";
 			return buffer;
 		}
 		// Set title window
@@ -84,7 +92,7 @@
 		else { window.parent.document.title = depth0.EventName + " - JourneyChurch.tv" }
 		
 		// Create the top title bar
-		buffer += "<div class=\"title-bar\">\n	<div class=\"container\">\n		<div class=\"grid_12\">\n			<h2>";
+		buffer += "<div class='title-bar'><div class='container'><div class='grid_12'><h2>";
 		if (stack1 = helpers.EventName) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
 		else { stack1 = depth0.EventName; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
 		buffer += escapeExpression(stack1)
