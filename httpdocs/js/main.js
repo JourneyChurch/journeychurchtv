7ccff9ec4c3f61290eaa875bf6fe2b54478479b1
Zac Conant <ZacConant@Journey-Zan-Conant.local>
2016-07-18 11:20:49 -0500

    Add overflowing for nav

--- httpdocs/js/main.js
+++ httpdocs/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $(".nav-icon").click(function () {
         $(".nav").fadeToggle(300);
-        $(".nav-icon").toggleClass("nav-visible");
+        $("body").toggleClass("nav-visible");
     });
 });
